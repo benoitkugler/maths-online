ac54a271420fe7d0f2c6b0a0faaa0c98708687c8
Benoit KUGLER <x.ben.x@free.fr>
2022-03-06 18:02:48 +0100

    better build

--- eleve/build_web.sh
+++ eleve/build_web.sh
@@ -3,4 +3,6 @@ echo "Moving build to server/static/eleve..." &&
 rm -r ../server/static/eleve/ && 
 mkdir ../server/static/eleve && 
 cp -r build/web/* ../server/static/eleve &&
+echo "Fixing bug https://github.com/flutter/flutter/issues/53745..." && 
+sed -i -e 's/return cache.addAll/cache.addAll/g' ../server/static/eleve/flutter_service_worker.js &&
 echo "Done."
