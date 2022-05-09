0040d24c5cdd4086c9a62ade3340e0e26472d187
Benoit KUGLER <x.ben.x@free.fr>
2022-05-09 17:37:26 +0200

    [DB] cleanup import script

---
+++ server/migrations/import_prod.sh
@@ -0,0 +1,19 @@
+# This script is a workaround for a bug related to user defined functions
+# It imports an SQL dump in two steps : functions and schema then data
+# The first argument is the name of the file dump
+#
+# The typical commands to run previoulsy are 
+# pg_dump -U <user> -h <host> -d education_prod > isyro.dump
+# scp <user>@<host>:isyro.dump isyro.dump
+
+echo "Spliting schema and data..." && 
+line=$(grep -n "COPY" $1 | cut -d: -f1 | head -1) &&  
+line="$((line-1))" && 
+(head -$line > schema.sql; cat > data.sql) < $1 && 
+echo "Resetting DB..." && 
+dropdb --if-exists isyro_prod && createdb isyro_prod && 
+echo "Importing..." && 
+psql isyro_prod < schema.sql && 
+psql isyro_prod < data.sql && 
+echo "Cleaning up" && 
+rm schema.sql data.sql
