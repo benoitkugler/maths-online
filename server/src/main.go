7861f6d87a7c73c57cb30637a94acbd677585304
Benoit KUGLER <x.ben.x@free.fr>
2022-05-30 17:16:36 +0200

    [server] remove recover

--- server/src/main.go
+++ server/src/main.go
@@ -169,7 +169,6 @@ func main() {
 	}
 	fmt.Println("Setup done (pending sanityChecks)")
 
-	e.Use(middleware.Recover())
 	err = e.Start(host) // start and block
 	e.Logger.Fatal(err) // report error and quit
 }
