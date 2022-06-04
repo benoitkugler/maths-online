1d075027ce093987a37db36dca1a9dcb696a7abd
Benoit KUGLER <x.ben.x@free.fr>
2022-06-04 12:24:04 +0200

    [trivial] enable question to be left

--- eleve/lib/trivialpoursuit/question.dart
+++ eleve/lib/trivialpoursuit/question.dart
@@ -1,5 +1,6 @@
 import 'package:eleve/build_mode.dart';
 import 'package:eleve/exercices/question.dart';
+import 'package:eleve/exercices/types.gen.dart';
 import 'package:eleve/quotes.dart';
 import 'package:flutter/material.dart';
 
@@ -14,14 +15,40 @@ class QuestionRoute extends StatelessWidget {
   const QuestionRoute(this.buildMode, this.question, this.onValid, {Key? key})
       : super(key: key);
 
+  Future<bool> _confirmCancel(BuildContext context) async {
+    final res = await showDialog<bool?>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Abandonner la question"),
+            content: const Text("Es-tu sur d'abandonner la question ?"),
+            actions: [
+              TextButton(
+                  child: const Text("Abandonner"),
+                  onPressed: () {
+                    Navigator.pop(context, true);
+                  })
+            ],
+          );
+        });
+    return res ?? false;
+  }
+
   @override
   Widget build(BuildContext context) {
     // make the route block until validated
     return WillPopScope(
-      onWillPop: () async => false,
+      onWillPop: () async {
+        final cancel = await _confirmCancel(context);
+        if (cancel) {
+          // send an empty response
+          onValid(ValidQuestionNotification(const QuestionAnswersIn({})));
+        }
+        return cancel;
+      },
       child: Scaffold(
         appBar: AppBar(
-          automaticallyImplyLeading: false,
+          automaticallyImplyLeading: true,
         ),
         body: QuestionW(
           buildMode,
