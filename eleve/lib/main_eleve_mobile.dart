0468bdf6b8b2cc661bb91e6d6ee88abbd05d1b5a
Benoit KUGLER <x.ben.x@free.fr>
2022-08-13 19:49:49 +0200

    [eleve] prepare build

--- eleve/lib/main_eleve_mobile.dart
+++ eleve/lib/main_eleve_mobile.dart
@@ -3,8 +3,8 @@ import 'package:eleve/main_eleve_shared.dart';
 import 'package:flutter/material.dart';
 import 'package:upgrader/upgrader.dart';
 
-// final bm = buildMode();
-final bm = BuildMode.dev;
+final bm = buildMode();
+// final bm = BuildMode.dev;
 
 void main() async {
   final audio = await loadAudioFromSettings();
