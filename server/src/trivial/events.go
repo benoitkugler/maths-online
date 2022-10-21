ae605268fa386c73118a0bb39c7037696f6f06bd
Benoit KUGLER <x.ben.x@free.fr>
2022-10-21 10:32:53 +0200

    [server/trivial] fix crash on all player disconnecting during question result

--- server/src/trivial/events.go
+++ server/src/trivial/events.go
@@ -77,8 +77,9 @@ func (r *Room) onTerminate() {
 var ErrGameStarted = errors.New("game already started")
 
 // Join should be used on a new connection, on one the of the following cases:
-//	- totally fresh connection
-//	- reconnection in an already started game
+//   - totally fresh connection
+//   - reconnection in an already started game
+//
 // It is safe for concurrent uses.
 // It returns an error for instance if the game has already started.
 func (r *Room) Join(player Player, connection Connection) error {
@@ -118,15 +119,24 @@ func (r *Room) Join(player Player, connection Connection) error {
 	} else { // reconnection
 		ProgressLogger.Printf("Game %s : reconnecting player %s...", r.ID, player.ID)
 
+		// if the game was started then temporary left by all players, trigger a new turn
+		triggerNewTurn := r.game.hasStarted() && r.nbActivePlayers() == 0
+
 		pc := r.players[player.ID]
 		pc.conn = connection // use the new client connection
 		pc.pl.Pseudo = player.Pseudo
 
-		event := PlayerReconnected{
+		events := Events{PlayerReconnected{
 			ID:     pc.pl.ID,
 			Pseudo: player.Pseudo,
+		}}
+		if triggerNewTurn {
+			ProgressLogger.Printf("Game %s : reviving by starting a new turn...", r.ID)
+
+			eventTurn := r.startTurn()
+			events = append(events, eventTurn)
 		}
-		r.broadcastEvents(Events{event})
+		r.broadcastEvents(events)
 	}
 
 	return nil
