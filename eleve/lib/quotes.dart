653fa72da0748d0464f02554ae953d7bddef32aa
Benoit KUGLER <x.ben.x@free.fr>
2022-10-24 14:38:54 +0200

    [eleve] attenuate blur effect, closes #88

--- eleve/lib/quotes.dart
+++ eleve/lib/quotes.dart
@@ -199,7 +199,7 @@ class Quote extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const shadows = [
-      BoxShadow(color: Colors.white, blurRadius: 7, spreadRadius: 3)
+      BoxShadow(color: Colors.white, blurRadius: 6, spreadRadius: 3)
     ];
     const fontSize = 16.0;
     return Padding(
