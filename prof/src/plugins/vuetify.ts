83e11f85e02768a98a5d374d2c2bdfc8bd85f25e
Benoit KUGLER <x.ben.x@free.fr>
2022-11-04 16:11:01 +0100

    [prof/editor] udate to new branding colors

--- prof/src/plugins/vuetify.ts
+++ prof/src/plugins/vuetify.ts
@@ -5,7 +5,23 @@ import { createVuetify } from "vuetify";
 import { aliases, mdi } from "vuetify/lib/iconsets/mdi";
 import "vuetify/styles";
 
+const myCustomLightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    primary: "#6fdec1",
+    "primary-darken-1": "#52a38e",
+    secondary: "#e8f241",
+    "secondary-darken-1": "#e5f02e"
+  }
+};
+
 export default createVuetify({
+  theme: {
+    defaultTheme: "myCustomLightTheme",
+    themes: {
+      myCustomLightTheme
+    }
+  },
   icons: {
     defaultSet: "mdi",
     aliases,
