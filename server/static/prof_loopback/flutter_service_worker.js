7764246454ad1fe99247631b8adb1b038fd841fa
Benoit KUGLER <x.ben.x@free.fr>
2022-12-19 20:00:52 +0100

    [eleve] tmp loopback build

--- server/static/prof_loopback/flutter_service_worker.js
+++ server/static/prof_loopback/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "manifest.json": "6be06c3ebad0d6a2ecb2cdd71690d463",
-"main.dart.js": "325c6ff29e6523f6cfd729a12479c128",
-"index.html": "abfe11fd61b35cf449d1af7d5471045a",
-"/": "abfe11fd61b35cf449d1af7d5471045a",
+"main.dart.js": "7bcd4c38056ba7dd356dcbdcb1d5d169",
+"index.html": "21646a5f862f32b13c6e0f24ece76a92",
+"/": "21646a5f862f32b13c6e0f24ece76a92",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
 "canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
