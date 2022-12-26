2e66a5b41699e3f85f87a7d8b3f7e71de4b8498f
Benoit KUGLER <x.ben.x@free.fr>
2022-12-26 14:00:35 +0100

    [prof/reviews] add triv config visualisation, closes #150

---
+++ prof/src/components/trivial/CategorieRow.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-list-item
+    rounded
+    :style="{
+      'border-color': colors[props.index],
+      borderWidth: '2px',
+      borderStyle: 'solid',
+    }"
+    class="my-2"
+  >
+    <slot></slot>
+  </v-list-item>
+</template>
+
+<script setup lang="ts">
+import { colorsPerCategorie } from "@/controller/trivial";
+
+interface Props {
+  index: number;
+}
+
+const props = defineProps<Props>();
+const colors = colorsPerCategorie;
+</script>
+
+<style scoped></style>
