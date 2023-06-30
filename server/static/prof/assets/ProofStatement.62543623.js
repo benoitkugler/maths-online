6b4fe85bfe28e8f3084795660c240d09a9016bf3
Benoit KUGLER <benoit.kugler@gmail.com>
2023-06-30 15:40:18 +0200

    [prof] tmp build

---
+++ server/static/prof/assets/ProofStatement.62543623.js
@@ -0,0 +1,1 @@
+import{d as n,o as m,c as u,_ as d}from"./index.a25720cc.js";const r=n({__name:"ProofStatement",props:{modelValue:{}},emits:["update:modelValue"],setup(o,{emit:t}){const a=o;return(s,e)=>(m(),u(d,{"model-value":a.modelValue.Content,"onUpdate:modelValue":e[0]||(e[0]=l=>t("update:modelValue",{Content:l}))},null,8,["model-value"]))}});export{r as default};
