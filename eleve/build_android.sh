284192d60b3b3a29f1d740a9bca19bedcc7ef7ff
Benoit KUGLER <benoit.kugler@gmail.com>
2023-07-02 14:45:53 +0200

    [eleve] document port forwarding command

--- eleve/build_android.sh
+++ eleve/build_android.sh
@@ -3,3 +3,6 @@
 # then press M
 # mv flutter_01.sksl.json flutter_android.sksl.json
 flutter build appbundle -t lib/main_eleve_mobile.dart --bundle-sksl-path flutter_android.sksl.json
+
+# NOTE: To test with localhost, use port forwarding
+# Android/Sdk/platform-tools/adb reverse tcp:1323 tcp:1323
