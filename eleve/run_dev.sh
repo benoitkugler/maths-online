ab72ee55d298b03e1ec753dc3796b44ccc60807f
Benoit KUGLER <benoit.kugler@gmail.com>
2023-08-12 12:18:20 +0200

    [eleve] add port fowarding to dev script

--- eleve/run_dev.sh
+++ eleve/run_dev.sh
@@ -1,2 +1,3 @@
 # run the main project in API dev mode, to test the app with a local server
+~/Android/Sdk/platform-tools/adb reverse tcp:1323 tcp:1323
 flutter run -t lib/main_eleve_mobile.dart --release --dart-define=mode=dev
