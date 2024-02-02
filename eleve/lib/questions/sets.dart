cad03f2693bdad97a29ec5d51558d0d956ac724a
Benoit KUGLER <benoit.kugler@gmail.com>
2024-02-02 15:45:17 +0100

    [eleve] add support for SetField

--- eleve/lib/questions/sets.dart
+++ eleve/lib/questions/sets.dart
@@ -1,32 +1,33 @@
-import 'package:eleve/questions/drag_text.dart';
 import 'package:eleve/questions/fields.dart';
 import 'package:eleve/types/src_maths_expression_sets.dart';
 import 'package:eleve/types/src_maths_questions_client.dart';
 import 'package:flutter/material.dart';
 
-class SetController extends FieldController {
-  final List<String> sets;
-  SetController(super.onChange, this.sets);
+// same as ListNode, but mutable
+class _Node {
+  List<_Node> args;
+  SetOp op;
+  Set leaf;
+  _Node(this.args, this.op, this.leaf);
 
-  ListNode answer = ListNode([], SetOp.sLeaf, 0);
+  factory _Node.empty() => _Node([], SetOp.sUnion, 0);
+  factory _Node.leaf(Set s) => _Node([], SetOp.sLeaf, s);
+  factory _Node.comp(_Node kid) => _Node([kid], SetOp.sComplement, 0);
 
-  @override
-  Answer getData() {
-    return SetAnswer(answer);
-  }
+  ListNode toListNode() =>
+      ListNode(args.map((e) => e.toListNode()).toList(), op, leaf);
 
-  @override
-  bool hasValidData() {
-    return answer.isValid();
-  }
+  factory _Node.fromListNode(ListNode n) =>
+      _Node(n.args.map((e) => _Node.fromListNode(e)).toList(), n.op, n.leaf);
 
-  @override
-  void setData(Answer answer) {
-    this.answer = (answer as SetAnswer).root;
-  }
-}
+  _Node shallowCopy() => _Node(args, op, leaf);
+  _Node deepCopy() => _Node(args.map((e) => e.deepCopy()).toList(), op, leaf);
 
-extension on ListNode {
+  bool get uOrI => op == SetOp.sUnion || op == SetOp.sInter;
+
+  // return true if the node is a sentinel empty value
+  bool isEmpty() => (op == SetOp.sUnion && args.isEmpty);
+
   bool isValid() {
     switch (op) {
       case SetOp.sUnion:
@@ -37,6 +38,135 @@ extension on ListNode {
         return args.length == 1 && args.first.isValid();
       case SetOp.sLeaf:
         return true;
+    }
+  }
+
+  // if the last element of args is empty, replace
+  void addOrReplace(_Node kid) {
+    if (args.isNotEmpty && args.last.isEmpty()) {
+      args[args.length - 1] = kid;
+    } else {
+      args.add(kid);
+    }
+  }
+
+  // replace all fields by other
+  void replace(_Node other) {
+    args = other.args;
+    op = other.op;
+    leaf = other.leaf;
+  }
+}
+
+class SetController extends FieldController {
+  final List<String> sets;
+
+  List<_Node> _history = []; // history API
+
+  _Node _answer = _Node.empty();
+  // pointer to one node of answer,
+  // may be modified by user tap
+  late _Node _cursor;
+
+  SetController(super.onChange, this.sets) {
+    _cursor = _answer; // start with focus on the root
+  }
+
+  @override
+  Answer getData() {
+    return SetAnswer(_answer.toListNode());
+  }
+
+  @override
+  bool hasValidData() {
+    return _answer.isValid();
+  }
+
+  @override
+  void setData(Answer answer) {
+    _answer = _Node.fromListNode((answer as SetAnswer).root);
+    _cursor = _answer; // reset the cursor to the root
+  }
+
+  void _saveHistory() {
+    _history.add(_answer.deepCopy());
+  }
+
+  void _restoreHistory() {
+    if (_history.isEmpty) return;
+    _answer = _history.last;
+    _cursor = _answer;
+    _history.removeLast();
+  }
+
+  void _clear() {
+    _saveHistory();
+    _answer = _Node.empty();
+    _cursor = _answer;
+  }
+
+  // add the given leaf to the current expression
+  void _addLeaf(Set s) {
+    _saveHistory();
+    if (_cursor.uOrI) {
+      // extend the union or intersection
+      _cursor.addOrReplace(_Node.leaf(s));
+    } else if (_cursor.op == SetOp.sComplement) {
+      // add or replace
+      _cursor.args = [_Node.leaf(s)];
+    } else {
+      // nothing to do
+    }
+  }
+
+  void _addParenthesis() {
+    _saveHistory();
+    if (_cursor.uOrI) {
+      if (_cursor.args.isNotEmpty && _cursor.args.last.isEmpty()) {
+        // change the cursor
+        _cursor = _cursor.args.last;
+      }
+    } else if (_cursor.op == SetOp.sComplement) {
+    } else if (_cursor.op == SetOp.sLeaf) {}
+  }
+
+  void _addOp(SetOp op) {
+    _saveHistory();
+    if (_cursor.uOrI) {
+      if (_cursor.op == op) {
+        // extend the union or intersection
+        _cursor.args.add(_Node.empty());
+      } else if (op == SetOp.sComplement) {
+        if (_cursor.args.isEmpty) {
+          // add a new node with comp and pass the focus
+          final newCursor = _Node.empty();
+          _cursor.args.add(_Node.comp(newCursor));
+          _cursor = newCursor;
+        } else if (_cursor.args.last.isEmpty()) {
+          // pass the focus
+          final newCursor = _Node.empty();
+          _cursor.args[_cursor.args.length - 1] = _Node.comp(newCursor);
+          _cursor = newCursor;
+        } else {
+          // apply comp to the current cursor
+          _cursor.replace(_Node.comp(_cursor.shallowCopy()));
+        }
+      } else {
+        // replace by the given op applied to this and empty
+        // and update the cursor
+        final newCursor = _Node.empty();
+        _cursor.replace(_Node([_cursor.shallowCopy(), newCursor], op, 0));
+        _cursor = newCursor;
+      }
+    } else if (_cursor.op == SetOp.sComplement) {
+      if (op == _cursor.op) {
+        // remove the comp
+        _cursor.replace(_cursor.args.isEmpty ? _Node.empty() : _cursor.args[0]);
+      } else {
+        _cursor.replace(_Node([_cursor.shallowCopy(), _Node.empty()], op, 0));
+      }
+    } else if (_cursor.op == SetOp.sLeaf && op == SetOp.sComplement) {
+      _cursor.replace(_Node.comp(_cursor.shallowCopy()));
     }
   }
 }
@@ -52,39 +182,134 @@ class SetFieldW extends StatefulWidget {
 }
 
 class _SetFieldWState extends State<SetFieldW> {
+  Offset? dragGesturePosition = Offset.zero;
+
   @override
   Widget build(BuildContext context) {
-    final sets = widget.controller.sets;
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        _NodeW(widget.controller.answer, sets),
-        // TODO: controls
-        Row(
-            children: List.generate(
-                sets.length,
-                (index) => DragText(index, [TextOrMath(sets[index], true)],
-                    enabled: true)).toList()),
-      ],
+    final ct = widget.controller;
+    const operators = [SetOp.sUnion, SetOp.sInter, SetOp.sComplement];
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              IconButton(
+                onPressed: () => setState(() => ct._clear()),
+                icon: const Icon(Icons.clear),
+                splashRadius: 20,
+              ),
+              Expanded(
+                child: Center(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: _NodeW(
+                        false, widget.controller._answer, ct.sets, ct._cursor,
+                        onTap: (cr) => setState(() => ct._cursor = cr)),
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: ct._history.isEmpty
+                    ? null
+                    : () => setState(() => ct._restoreHistory()),
+                icon: const Icon(Icons.undo),
+                splashRadius: 20,
+              )
+            ],
+          ),
+          Wrap(
+              children: List.generate(
+            ct.sets.length,
+            (index) => _Control(
+              () => setState(() => ct._addLeaf(index)),
+              ct.sets[index],
+            ),
+          ).toList()),
+          Row(mainAxisSize: MainAxisSize.min, children: [
+            _Control(() => setState(() => ct._addParenthesis()), '()'),
+            ...operators.map((op) =>
+                _Control(() => setState(() => ct._addOp(op)), op.latex()))
+          ]),
+          // DEBUG only
+          // ElevatedButton(
+          //     onPressed: widget.controller.hasValidData()
+          //         ? () {
+          //             setState(() {
+          //               widget.controller
+          //                   .setData(SetAnswer(_Node.empty().toListNode()));
+          //             });
+          //           }
+          //         : null,
+          //     child: const Text("Valid"))
+        ],
+      ),
+    );
+  }
+}
+
+extension on SetOp {
+  String latex() {
+    switch (this) {
+      case SetOp.sUnion:
+        return "\\cup";
+      case SetOp.sInter:
+        return "\\cap";
+      case SetOp.sComplement:
+        return "\\overline{ \\quad }";
+      case SetOp.sLeaf:
+        return "";
+    }
+  }
+}
+
+class _Control extends StatelessWidget {
+  final void Function() onTap;
+  final String latex;
+  const _Control(this.onTap, this.latex, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: ElevatedButton(
+          onPressed: onTap,
+          style: ElevatedButton.styleFrom(
+            elevation: 4,
+            backgroundColor: Colors.black54,
+            visualDensity: VisualDensity.comfortable,
+          ),
+          child: textMath(latex, const TextStyle(fontSize: 16))),
     );
   }
 }
 
 // wrapper dispatching the concrete type
 class _NodeW extends StatelessWidget {
-  final ListNode node;
+  final bool showParenthesis;
+  final _Node node;
   final List<String> sets;
+  final _Node cursor;
 
-  const _NodeW(this.node, this.sets, {super.key});
+  final void Function(_Node) onTap;
+
+  const _NodeW(this.showParenthesis, this.node, this.sets, this.cursor,
+      {required this.onTap, super.key});
 
   Widget kid() {
     switch (node.op) {
       case SetOp.sUnion:
-        return _UnionW(node.args, sets);
+        return _UniOrInt("\\cup", showParenthesis, node.args, sets, cursor,
+            onTap: onTap);
       case SetOp.sInter:
-        return _InterW(node.args, sets);
+        return _UniOrInt("\\cap", showParenthesis, node.args, sets, cursor,
+            onTap: onTap);
       case SetOp.sComplement:
-        return _ComplementW(node.args.isEmpty ? null : node.args.first, sets);
+        return _ComplementW(
+            node.args.isEmpty ? null : node.args.first, sets, cursor,
+            onTap: onTap);
       case SetOp.sLeaf:
         return _LeafW(node.leaf, sets);
     }
@@ -92,75 +317,85 @@ class _NodeW extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: const EdgeInsets.all(4), child: kid());
+    final isCursor = node == cursor;
+    return InkWell(
+      onTap: () => onTap(node),
+      child: Padding(
+        padding: node.op == SetOp.sLeaf
+            ? const EdgeInsets.symmetric(vertical: 2)
+            : const EdgeInsets.all(2),
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(4)),
+              color: isCursor
+                  ? Colors.white.withOpacity(0.2)
+                  : Colors.transparent),
+          child: kid(),
+        ),
+      ),
+    );
   }
 }
 
-const textStyle = TextStyle(fontSize: 16);
+const textStyle = TextStyle(fontSize: 20);
 
-class _UnionW extends StatelessWidget {
-  final List<ListNode> args;
+class _UniOrInt extends StatelessWidget {
+  final String operator;
+  final bool showParenthesis;
+  final List<_Node> args;
   final List<String> sets;
+  final _Node cursor;
 
-  const _UnionW(this.args, this.sets, {super.key});
+  final void Function(_Node) onTap;
+
+  const _UniOrInt(
+      this.operator, this.showParenthesis, this.args, this.sets, this.cursor,
+      {required this.onTap, super.key});
 
   @override
   Widget build(BuildContext context) {
-    // add the U sign
-    final l = <Widget>[];
-    for (var i = 0; i < args.length; i++) {
-      l.add(_NodeW(args[i], sets));
-      if (i != args.length - 1) {
-        l.add(textMath("\\cup", textStyle));
-      }
+    if (args.isEmpty) {
+      // display an empty square
+      return const SizedBox(height: 20, width: 10);
     }
-    if (args.length >= 2) {
-      // add ()
-      l.insert(0, const Text("("));
-      l.add(const Text(")"));
-    }
-    return Row(children: l);
-  }
-}
-
-class _InterW extends StatelessWidget {
-  final List<ListNode> args;
-  final List<String> sets;
-
-  const _InterW(this.args, this.sets, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
     // add the n sign
     final l = <Widget>[];
     for (var i = 0; i < args.length; i++) {
-      l.add(_NodeW(args[i], sets));
+      l.add(_NodeW(true, args[i], sets, cursor, onTap: onTap));
       if (i != args.length - 1) {
-        l.add(textMath("\\cap", textStyle));
+        l.add(textMath(operator, textStyle));
       }
     }
-    if (args.length >= 2) {
+    if (args.length >= 2 && showParenthesis) {
       // add ()
-      l.insert(0, const Text("("));
-      l.add(const Text(")"));
+      l.insert(0, textMath("(", textStyle));
+      l.add(textMath(")", textStyle));
     }
-    return Row(children: l);
+    return Row(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.baseline,
+        textBaseline: TextBaseline.alphabetic,
+        children: l);
   }
 }
 
 class _ComplementW extends StatelessWidget {
-  final ListNode? arg;
+  final _Node? arg;
   final List<String> sets;
+  final _Node cursor;
 
-  const _ComplementW(this.arg, this.sets, {super.key});
+  final void Function(_Node) onTap;
+
+  const _ComplementW(this.arg, this.sets, this.cursor,
+      {required this.onTap, super.key});
 
   @override
   Widget build(BuildContext context) {
     final color = Theme.of(context).textTheme.bodyMedium?.color ?? Colors.black;
     return Container(
       decoration: BoxDecoration(border: Border(top: BorderSide(color: color))),
-      // padding: const EdgeInsets.only(top: 2),
-      child: arg == null ? null : _NodeW(arg!, sets),
+      child:
+          arg == null ? null : _NodeW(false, arg!, sets, cursor, onTap: onTap),
     );
   }
 }
