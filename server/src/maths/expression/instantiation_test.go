benoitkugler/maths-online

commit 4837d17b30c0f0f58c8184712195157f7f80643c

[expression] use rationnal in matrices operations, closes #320

server/src/maths/expression/eval.go
@@ -773,11 +773,24 @@ func (r real) toExpr() *Expr {
 }
 
 func sumReal(r1, r2 real) real {
+	r1.add(r2)
+	return r1
+}
+
+func (r1 *real) add(r2 real) {
 	if r1.isRational && r2.isRational {
-		return real{isRational: true, rat: sumRat(r1.rat, r2.rat)}
+		r1.rat = sumRat(r1.rat, r2.rat)
+	} else {
+		// use eval to handle the case where r1 or r2 is rational
+		r1.isRational = false
+		r1.val = r1.eval() + r2.eval()
 	}
-	// use eval to handle the case where r1 or r2 is rational
-	return real{isRational: false, val: r1.eval() + r2.eval()}
+}
+
+// transforms r to -r
+func (r *real) opposite() {
+	r.val = -r.val
+	r.rat.p = -r.rat.p
 }
 
 // return r1 - r2

server/src/maths/expression/instantiation.go
@@ -252,7 +252,7 @@ func (expr *Expr) instantiate(ctx *resolver) (*Expr, error) {
 			if err != nil {
 				return nil, err
 			}
-			return newNb(d), nil
+			return d.toExpr(), nil
 		case transposeFn:
 			mat, ok := right.atom.(matrix)
 			if !ok {

server/src/maths/expression/matrices.go
@@ -76,7 +76,7 @@ func (A matrix) minus(B matrix) (matrix, error) {
 
 // numberMatrix is an efficient version of a matrix
 // containing only scalars
-type numberMatrix [][]float64
+type numberMatrix [][]real
 
 func (A numberMatrix) dims() (rows, cols int) {
 	if len(A) == 0 {
@@ -87,9 +87,12 @@ func (A numberMatrix) dims() (rows, cols int) {
 
 func newZeros(n, m int) numberMatrix {
 	out := make(numberMatrix, n)
-	vals := make([]float64, n*m)
+	buffer := make([]real, n*m)
+	for j := range buffer {
+		buffer[j] = newRealInt(0)
+	}
 	for i := range out {
-		out[i] = vals[i*m : (i+1)*m]
+		out[i] = buffer[i*m : (i+1)*m]
 	}
 	return out
 }
@@ -99,9 +102,9 @@ func newNumberMatrixFrom(A matrix) (numberMatrix, bool) {
 	out := newZeros(n, m)
 	for i, row := range A {
 		for j, v := range row {
-			var ok bool
-			out[i][j], ok = v.isConstantTerm()
-			if !ok {
+			var err error
+			out[i][j], err = v.evalReal(nil)
+			if err != nil {
 				return nil, false
 			}
 		}
@@ -114,7 +117,7 @@ func (A numberMatrix) toExprMatrix() matrix {
 	out := newMatrixEmpty(n, m)
 	for i, row := range A {
 		for j, v := range row {
-			out[i][j] = NewNb(v)
+			out[i][j] = v.toExpr()
 		}
 	}
 	return out
@@ -124,11 +127,11 @@ func (A numberMatrix) toExprMatrix() matrix {
 func (A numberMatrix) prodTo(B, out numberMatrix) {
 	for i, row := range out {
 		for j := range row {
-			s := 0.
+			s := newRealInt(0)
 			for k := range A[i] {
 				Aik := A[i][k]
 				Bkj := B[k][j]
-				s += Aik * Bkj
+				s.add(multReal(Aik, Bkj))
 			}
 			out[i][j] = s
 		}
@@ -166,7 +169,7 @@ func (A numberMatrix) pow(nf float64) (numberMatrix, error) {
 	for i := range out {
 		for j := range out {
 			if i == j {
-				out[i][j] = 1
+				out[i][j] = newRealInt(1)
 			}
 		}
 	}
@@ -190,34 +193,34 @@ func (A numberMatrix) pow(nf float64) (numberMatrix, error) {
 	return out, nil
 }
 
-func (A numberMatrix) determinant() (float64, error) {
+func (A numberMatrix) determinant() (real, error) {
 	m, n := A.dims()
 	if m != n {
-		return 0, fmt.Errorf("déterminant d'une matrice non carrée (%d, %d)", m, n)
+		return real{}, fmt.Errorf("déterminant d'une matrice non carrée (%d, %d)", m, n)
 	}
 
 	if m == 0 {
-		return 0, nil
+		return newRealInt(0), nil
 	} else if m == 1 {
 		return A[0][0], nil
 	} else if m == 2 {
-		return A[0][0]*A[1][1] - A[1][0]*A[0][1], nil
+		return minusReal(multReal(A[0][0], A[1][1]), multReal(A[1][0], A[0][1])), nil
 	} else { // last row dev
-		det := 0.
+		det := newRealInt(0)
 		for j := range A {
 			minor := A.minor(m-1, j)
 			coeff := A[m-1][j]
-			det += coeff * minor
+			det.add(multReal(coeff, minor))
 		}
 		return det, nil
 	}
 }
 
 // assume A is squared
-func (A numberMatrix) minor(i, j int) float64 {
+func (A numberMatrix) minor(i, j int) real {
 	minor, _ := A.submatrix(i, j).determinant() // determinant is safe here
 	if (i+j)%2 != 0 {                           // i and j is shifted by one, compensating
-		minor = -minor
+		minor.opposite()
 	}
 	return minor
 }
@@ -230,7 +233,7 @@ func (A numberMatrix) submatrix(i, j int) numberMatrix {
 		if i2 == i { // remove row i
 			continue
 		}
-		newRow := make([]float64, len(A)-1)
+		newRow := make([]real, len(A)-1)
 		copy(newRow, row[:j])
 		copy(newRow[j:], row[j+1:])
 		out = append(out, newRow)
@@ -245,13 +248,13 @@ func (A numberMatrix) invert() (numberMatrix, error) {
 	if err != nil {
 		return nil, err
 	}
-	if det == 0 {
+	if det.eval() == 0 {
 		return nil, errors.New("matrice non inversible (déterminant nul)")
 	}
 	inv := newZeros(len(A), len(A))
 	for i, row := range inv {
 		for j := range row {
-			row[j] = A.minor(j, i) / det // tranpose
+			row[j] = divReal(A.minor(j, i), det) // tranpose
 		}
 	}
 	return inv, nil

server/src/maths/expression/matrices_test.go
@@ -33,7 +33,7 @@ func Test_matricesOperations(t *testing.T) {
 		{"-A", matrix{{newNb(-1), newNb(-2)}, {newNb(-3), newNb(-4)}}},
 		{"trans(A)", matrix{{newNb(1), newNb(3)}, {newNb(2), newNb(4)}}},
 		{"transpose(A)", matrix{{newNb(1), newNb(3)}, {newNb(2), newNb(4)}}},
-		{"inv(D)", matrix{{newNb(0.5), newNb(0)}, {newNb(0), newNb(0.2)}}},
+		{"inv(D)", matrix{{rat{1, 2}.toExpr(), newNb(0)}, {newNb(0), rat{1, 5}.toExpr()}}},
 	}
 
 	for i, op := range ops {
@@ -147,7 +147,7 @@ func Test_matrix_determinant(t *testing.T) {
 			t.Errorf("matrix.determinant() error = %v, wantErr %v", err, tt.wantErr)
 			return
 		}
-		if got != tt.want {
+		if got.eval() != tt.want {
 			t.Errorf("matrix.determinant() = %v, want %v", got, tt.want)
 		}
 	}
@@ -181,7 +181,7 @@ func Test_matrix_invert(t *testing.T) {
 			return
 		}
 		want, _ := newNumberMatrixFrom(mustParse(t, tt.want).atom.(matrix))
-		if !tt.wantErr && !reflect.DeepEqual(got, want) {
+		if !tt.wantErr && !reflect.DeepEqual(got.toExprMatrix(), want.toExprMatrix()) {
 			t.Errorf("matrix.invert() = %v, want %v", got, want)
 		}
 	}

commit 5c2497d24efbe164ded27b915fcdf3e0cfd96930

[expression] fix bug in eval introduced by 4837d17b30c0f0f58c8184712195157f7f80643c

server/src/maths/expression/eval.go
@@ -387,6 +387,7 @@ func (fn function) eval(_, right *Expr, b *resolver) (real, error) {
 	case floorFn:
 		return newRealInt(int(math.Floor(arg))), nil
 	case sqrtFn:
+		fmt.Println(arg)
 		return newReal(math.Sqrt(arg)), nil
 	case sgnFn:
 		if arg > 0 {
@@ -782,8 +783,8 @@ func (r1 *real) add(r2 real) {
 		r1.rat = sumRat(r1.rat, r2.rat)
 	} else {
 		// use eval to handle the case where r1 or r2 is rational
-		r1.isRational = false
 		r1.val = r1.eval() + r2.eval()
+		r1.isRational = false
 	}
 }
 

server/src/maths/expression/instantiation_test.go
@@ -1,6 +1,7 @@
 package expression
 
 import (
+	"math"
 	"reflect"
 	"testing"
 
@@ -338,4 +339,16 @@ func TestInstantiateWithIntrinsics(t *testing.T) {
 	tu.Assert(t, ok)
 	_, ok = IsInt(bn)
 	tu.Assert(t, ok)
+}
+
+func TestCos(t *testing.T) {
+	e := mustParse(t, "sqrt(16+9-2*4*3*cos(27*pi/180))")
+	v, err := e.Evaluate(Vars{})
+	tu.AssertNoErr(t, err)
+	tu.Assert(t, !math.IsNaN(v))
+
+	e = mustParse(t, "sqrt(b*b+c*c-2*b*c*cos(d*pi/180))")
+	v, err = e.Evaluate(Vars{NewVar('b'): newNb(4), NewVar('c'): newNb(3), NewVar('d'): newNb(27)})
+	tu.AssertNoErr(t, err)
+	tu.Assert(t, !math.IsNaN(v))
 }